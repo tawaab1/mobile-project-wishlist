eccd45d2b1a18c9b24efddcc2f34ac36d002acb4
jommel amor <jommel>
2020-06-16 18:58:55 +1200

    Made the Wishlist activity working

--- 10-fragment-&-dialog-fragment/app/src/main/java/graysono/com/cp09progressdialogwebview/activities/WishlistActivity.kt
+++ 10-fragment-&-dialog-fragment/app/src/main/java/graysono/com/cp09progressdialogwebview/activities/WishlistActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import graysono.com.cp09progressdialogwebview.R
+import kotlinx.android.synthetic.main.activity_wishlist.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class WishlistActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wishlist)
+
+        bnv2.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener())
+        bnv2.menu.getItem(1).isChecked = true
     }
 
-    
+
 
 
     inner class OnNavigationItemSelectedListener :
@@ -31,11 +36,11 @@ class WishlistActivity : BaseActivity() {
                 /**
                  * navigates to the About Us activity
                  */
-//                R.id.navigation_profile ->{
-//                    startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
-//                    finish()
-//                    true
-//                }
+                R.id.navigation_profile ->{
+                    startActivity(Intent(this@WishlistActivity, ProfileActivity::class.java))
+                    finish()
+                    true
+                }
                 R.id.navigation_wishlist -> {
                     startActivity(Intent(this@WishlistActivity, WishlistActivity::class.java))
                     finish()
