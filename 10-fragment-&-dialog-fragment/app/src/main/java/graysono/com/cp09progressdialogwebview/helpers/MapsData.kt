a2274aea7648c9ffaaf1dcf9465d1ca7a3c679b9
nukar1 <nukanukaroy@hotmail.com>
2020-06-17 16:24:50 +1200

    finished coding, need to fix error

---
+++ 10-fragment-&-dialog-fragment/app/src/main/java/graysono/com/cp09progressdialogwebview/helpers/MapsData.kt
@@ -0,0 +1,10 @@
+package graysono.com.cp09progressdialogwebview.helpers
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.StreetViewPanoramaLocation
+
+class MapsData(
+    var name: String,
+    var location: LatLng,
+    var drawable: Int
+)
